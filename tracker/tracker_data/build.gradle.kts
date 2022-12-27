528af1509a0d5b158f2151904901fe8b32c12f71
Mikhail Kirilkin <m.kirilkin@bsl.dev>
2022-12-28 01:04:56 +0700

    migrate gradle to kotlin dsl. Use base-module.gradle and compose-module.gradle

---
+++ tracker/tracker_data/build.gradle.kts
@@ -0,0 +1,11 @@
+apply {
+    from("$rootDir/base-module.gradle")
+}
+
+dependencies {
+    "implementation"(project(Modules.core))
+    "implementation"(project(Modules.trackerDomain))
+
+    addRetrofitDependencies()
+    addRoomDependencies()
+}
