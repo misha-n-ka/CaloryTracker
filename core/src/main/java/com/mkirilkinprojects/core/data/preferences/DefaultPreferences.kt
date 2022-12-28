a32f4f216ed9bf5a3e35322ef4f9f801973c87b0
Mikhail Kirilkin <m.kirilkin@bsl.dev>
2022-12-29 00:33:55 +0700

    create interface and implementation for shared preferences

---
+++ core/src/main/java/com/mkirilkinprojects/core/data/preferences/DefaultPreferences.kt
@@ -0,0 +1,101 @@
+package com.mkirilkinprojects.core.data.preferences
+
+import android.content.SharedPreferences
+import com.mkirilkinprojects.core.domain.models.ActivityLevel
+import com.mkirilkinprojects.core.domain.models.Gender
+import com.mkirilkinprojects.core.domain.models.GoalType
+import com.mkirilkinprojects.core.domain.models.UserInfo
+import com.mkirilkinprojects.core.domain.preferences.Preferences
+
+class DefaultPreferences(
+    private val sharedPref: SharedPreferences
+) : Preferences {
+    override fun saveGender(gender: Gender) {
+        sharedPref.edit()
+            .putString(KEY_GENDER, gender.name)
+            .apply()
+    }
+
+    override fun saveAge(age: Int) {
+        sharedPref.edit()
+            .putInt(KEY_AGE, age)
+            .apply()
+    }
+
+    override fun saveWeight(weight: Float) {
+        sharedPref.edit()
+            .putFloat(KEY_WEIGHT, weight)
+            .apply()
+    }
+
+    override fun saveHeight(height: Int) {
+        sharedPref.edit()
+            .putInt(KEY_HEIGHT, height)
+            .apply()
+    }
+
+    override fun saveActivityLevel(level: ActivityLevel) {
+        sharedPref.edit()
+            .putString(KEY_ACTIVITY_LEVEL, level.name)
+            .apply()
+    }
+
+    override fun saveGoalType(type: GoalType) {
+        sharedPref.edit()
+            .putString(KEY_GOAL_TYPE, type.name)
+            .apply()
+    }
+
+    override fun saveCarbRatio(ratio: Float) {
+        sharedPref.edit()
+            .putFloat(KEY_CARB_RATIO, ratio)
+            .apply()
+    }
+
+    override fun saveProteinRatio(ratio: Float) {
+        sharedPref.edit()
+            .putFloat(KEY_PROTEIN_RATIO, ratio)
+            .apply()
+    }
+
+    override fun saveFatRatio(ratio: Float) {
+        sharedPref.edit()
+            .putFloat(KEY_FAT_RATIO, ratio)
+            .apply()
+    }
+
+    override fun loadUserInfo(): UserInfo {
+        val age = sharedPref.getInt(KEY_AGE, -1)
+        val height = sharedPref.getInt(KEY_HEIGHT, -1)
+        val weight = sharedPref.getFloat(KEY_WEIGHT, -1f)
+        val genderString = sharedPref.getString(KEY_GENDER, null)
+        val activityLevelString = sharedPref.getString(KEY_ACTIVITY_LEVEL, null)
+        val goalTypeString = sharedPref.getString(KEY_GOAL_TYPE, null)
+        val carbRatio = sharedPref.getFloat(KEY_CARB_RATIO, -1f)
+        val proteinRatio = sharedPref.getFloat(KEY_PROTEIN_RATIO, -1f)
+        val fatRatio = sharedPref.getFloat(KEY_FAT_RATIO, -1f)
+        return UserInfo(
+            gender = Gender.fromString(genderString),
+            age = age,
+            weight = weight,
+            height = height,
+            activityLevel = ActivityLevel.fromString(activityLevelString),
+            goalType = GoalType.fromString(goalTypeString),
+            carbRatio = carbRatio,
+            proteinRatio = proteinRatio,
+            fatRatio = fatRatio
+        )
+    }
+
+    companion object {
+        const val KEY_GENDER = "gender"
+        const val KEY_AGE = "age"
+        const val KEY_WEIGHT = "weight"
+        const val KEY_HEIGHT = "height"
+        const val KEY_ACTIVITY_LEVEL = "activity_level"
+        const val KEY_GOAL_TYPE = "goal_type"
+        const val KEY_CARB_RATIO = "carb_ratio"
+        const val KEY_PROTEIN_RATIO = "protein_ratio"
+        const val KEY_FAT_RATIO = "fat_ratio"
+    }
+}
---
+++ core/src/main/java/com/mkirilkinprojects/core/domain/preferences/Preferences.kt
@@ -0,0 +1,21 @@
+package com.mkirilkinprojects.core.domain.preferences
+
+import com.mkirilkinprojects.core.domain.models.ActivityLevel
+import com.mkirilkinprojects.core.domain.models.Gender
+import com.mkirilkinprojects.core.domain.models.GoalType
+import com.mkirilkinprojects.core.domain.models.UserInfo
+
+interface Preferences {
+
+    fun saveGender(gender: Gender)
+    fun saveAge(age: Int)
+    fun saveWeight(weight: Float)
+    fun saveHeight(height: Int)
+    fun saveActivityLevel(level: ActivityLevel)
+    fun saveGoalType(type: GoalType)
+    fun saveCarbRatio(ratio: Float)
+    fun saveProteinRatio(ratio: Float)
+    fun saveFatRatio(ratio: Float)
+
+    fun loadUserInfo(): UserInfo
+}
