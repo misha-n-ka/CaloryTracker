38c267a56e16265e3e8f1139853a7064733e866d
Mikhail Kirilkin <m.kirilkin@bsl.dev>
2023-01-04 13:05:31 +0700

    create TrackerOverviewScreen with NutrientsHeader

---
+++ tracker/tracker_presentation/src/main/java/com/mkirilkinprojects/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -0,0 +1,31 @@
+package com.mkirilkinprojects.tracker_presentation.tracker_overview
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.mkirilkinprojects.core.util.UiEvent
+import com.mkirilkinprojects.core_ui.LocalSpacing
+import com.mkirilkinprojects.tracker_presentation.tracker_overview.components.NutrientsHeader
+
+@Composable
+fun TrackerOverViewScreen(
+    onNavigate: (UiEvent.Navigate) -> Unit,
+    viewModel: TrackerOverviewViewModel = hiltViewModel()
+) {
+    val spacing = LocalSpacing.current
+    val state = viewModel.state
+    val context = LocalContext.current
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(bottom = spacing.spaceMedium)
+    ) {
+        item {
+            NutrientsHeader(state = state)
+        }
+    }
+}
