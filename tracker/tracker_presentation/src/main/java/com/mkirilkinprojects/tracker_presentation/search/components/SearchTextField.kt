6a0a2c181ea0236a7f5ff41d38509979f74f4446
Mikhail Kirilkin <m.kirilkin@bsl.dev>
2023-01-06 21:33:37 +0700

    fix trackfood amount keyboard option = Number

--- tracker/tracker_presentation/src/main/java/com/mkirilkinprojects/tracker_presentation/search/components/SearchTextField.kt
+++ tracker/tracker_presentation/src/main/java/com/mkirilkinprojects/tracker_presentation/search/components/SearchTextField.kt
@@ -25,7 +25,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.mkirilkinprojects.core_ui.LocalSpacing
 import com.mkirilkinprojects.tracker_presentation.R
@@ -55,8 +54,7 @@ fun SearchTextField(
                 }
             ),
             keyboardOptions = KeyboardOptions(
-                imeAction = ImeAction.Search,
-                keyboardType = KeyboardType.Number
+                imeAction = ImeAction.Search
             ),
             modifier = Modifier
                 .clip(RoundedCornerShape(5.dp))
