58ad463299470c66c01fff01d22781beabfce8a4
Mikhail Kirilkin <m.kirilkin@bsl.dev>
2023-01-08 02:29:49 +0700

    create and add to gradle HiltTestRunner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         versionCode = ProjectConfig.versionCode
         versionName = ProjectConfig.versionName
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "com.mkirilkinprojects.calorytracker.HiltTestRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
